7d052715f61b7afe5d419ed7c690837d7c818c5a
hellboy2310 <bhaveshpurohit2310@gmail.com>
2022-08-28 23:06:52 +0530

    jira clone completed

--- script.js
+++ script.js
@@ -2,10 +2,127 @@ let addBtn = document.querySelector(".addBtn");
 let isBtnPressed = false;
 let body = document.querySelector('body');
 let taskListsBtn = document.querySelector(".taskLists");
-let todoArr = [];
-let completedArr = [];
+let todoArr = localStorage.getItem("todoArr");
+let completedArr = localStorage.getItem("completedArr");
+let completedBtn  = document.querySelector(".completed");
 
 
+if(!completedArr){
+    localStorage.setItem("completedArr","[]");
+}
+
+if(todoArr){
+
+}
+
+else{
+    todoArr = [];
+todoArr = JSON.stringify(todoArr);
+localStorage.setItem("todoArr",todoArr);
+
+
+}
+//here we will add the functionality on the tasklists side
+
+document.addEventListener('DOMContentLoaded', function() {
+    // console.log('DOM fully loaded and parsed');
+    taskListsBtn.click();
+});
+
+
+
+taskListsBtn.addEventListener("click",function(){
+    if(!taskListsBtn.classList.contains("active")){
+        taskListsBtn.classList.add("active");
+        completedBtn.classList.remove("active");
+    }
+
+    let allTaskDiv = document.querySelectorAll(".oneTask");
+    for(let i = 0;i<allTaskDiv.length;i++){
+        allTaskDiv[i].remove();
+    }
+
+    todoArr = JSON.parse(localStorage.getItem("todoArr"));
+
+
+    for(let i = 0;i<todoArr.length;i++)
+    {
+        let divEle = document.createElement("div");
+        divEle.setAttribute("class","oneTask");
+        divEle.innerHTML = `<i class = "material-icons done">done</i>
+        <p>${todoArr[i]}</p>
+        <i class = "material-icons delete">delete</i>`
+        
+
+        let deleteBtn = divEle.querySelector(".delete");
+        deleteBtn.addEventListener("click",function(e){
+            e.target.parentNode.remove();
+            let allP = document.querySelectorAll(".oneTask p");
+            let newArr = [];
+            for(let i = 0;i<allP.length;i++){
+                newArr.push(allP[i].innerHTML);
+            }
+            localStorage.setItem("todoArr",JSON.stringify(newArr));
+            console.log(newArr);
+        })
+   
+        let doneBtn = divEle.querySelector(".done");
+        doneBtn.addEventListener("click",function(e){
+            let taskValue = e.target.nextElementSibling.innerHTML;
+            e.target.parentNode.remove();
+            let allP = document.querySelectorAll(".oneTask p");
+            let newArr = [];
+            for(let i = 0;i<allP.length;i++){
+                newArr.push(allP[i].innerHTML);
+            }
+          
+            localStorage.setItem("todoArr",JSON.stringify(newArr));
+            let completedArr =JSON.parse(localStorage.getItem("completedArr"));
+            completedArr.push(taskValue);  
+            console.log(completedArr);
+            localStorage.setItem("completedArr",JSON.stringify(completedArr))
+        })
+        let parentDiv = document.querySelector(".tasks");
+        parentDiv.appendChild(divEle);
+   }
+    //here we will get all the tasks which were present inside the tasklists section   
+    console.log(todoArr);
+})
+
+completedBtn.addEventListener("click",function(){
+    if(!completedBtn.classList.contains("active")){
+        completedBtn.classList.add("active");
+        taskListsBtn.classList.remove("active");
+    }
+
+    let allTaskDiv = document.querySelectorAll(".oneTask");
+    for(let i = 0;i<allTaskDiv.length;i++){
+        allTaskDiv[i].remove();
+    }    
+    let completedArr = JSON.parse(localStorage.getItem("completedArr"));
+     for(let i = 0;i<completedArr.length;i++){
+        let divEle = document.createElement("div");
+        divEle.setAttribute("class","oneTask");
+        divEle.innerHTML = `<p>${completedArr[i]}</p>
+        <i class = "material-icons delete">delete</i>`
+        
+        
+        let deleteBtn = divEle.querySelector(".delete");
+        deleteBtn.addEventListener("click",function(e){
+            e.target.parentNode.remove();
+            let allP = document.querySelectorAll(".oneTask p");
+            for(let i = 0;i<allP.length;i++){
+                newArr.push(allP[i].innerHTML);
+            }
+            localStorage.setItem("completedArr",JSON.stringify(newArr));
+        })
+        
+
+        let parentDiv = document.querySelector(".tasks");
+        parentDiv.appendChild(divEle);
+     }
+
+})
 
 
 
@@ -28,10 +145,11 @@ addBtn.addEventListener("click",function(){
                 <p>${e.target.value}</p>
                 <i class = "material-icons delete">delete</i>`
                 //this will add the task in the todo arr
+                todoArr =JSON.parse(localStorage.getItem("todoArr"));
                 todoArr.push(e.target.value);
-
+                localStorage.setItem("todoArr",JSON.stringify(todoArr));
                 divToAdd.textContent = "";
-                console.log(todoArr);
+                // console.log(todoArr);
              
                 //this will delete the task from ui
                 let deleteBtn = divEle.querySelector(".delete");
@@ -44,7 +162,9 @@ addBtn.addEventListener("click",function(){
                 {
                     newArr.push(allP[i].innerHTML);
                 }
-                console.log(newArr);
+                localStorage.setItem("todoArr",JSON.stringify(newArr));
+
+                // console.log(newArr);
                 
                 })
 
@@ -60,16 +180,20 @@ addBtn.addEventListener("click",function(){
                     {
                         newArr.push(allP[i].innerHTML);
                     }
+                    localStorage.setItem("todoArr",JSON.stringify(newArr));
+                    let completedArr =JSON.parse(localStorage.getItem("completedArr"));
                     completedArr.push(taskValue);
-                    console.log(newArr);
-                    console.log(completedArr);
+                    localStorage.setItem("completedArr",JSON.stringify(completedArr));
+                    // console.log(newArr);
+                    // console.log(completedArr);
 
                 })
-
-
                 let parentDiv  = document.querySelector(".tasks");
                 parentDiv.appendChild(divEle);
+                taskListsBtn.click();
+                addBtn.click(); 
             }
+            
         })
         isBtnPressed = true;
 
