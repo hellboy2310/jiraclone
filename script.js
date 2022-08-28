a0027650287075a7eb3b682054a7b0f8a7fdc414
hellboy2310 <bhaveshpurohit2310@gmail.com>
2022-08-28 17:47:05 +0530

    styling done

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ let addBtn  = document.querySelector(".addBtn");
 let addModalContainter = document.querySelector(".tasks");
 let flag = true;
 let taskAreacontainer  = document.querySelector(".taskarea");
+let mainCont = document.querySelector(".container");
+
+
 
 addBtn.addEventListener("click",function(){
     if(flag){
@@ -13,9 +16,27 @@ addBtn.addEventListener("click",function(){
    flag = !flag;
 })
 
-taskAreacontainer.addEventListener("keydown",function(e){
-    let key = e.target.value;
+addModalContainter.addEventListener("keydown",function(e){
+    let key = e.key;
+    if(key == "Enter"){
+        createTicket(taskAreacontainer.value);
+        taskAreacontainer.value = "";
+        addModalContainter.style.display = 'none';
+        flag = !flag;
+    }
+
+
+function  createTicket(task){
+        let ticketCont = document.createElement('div');
+        ticketCont.setAttribute('class','ticket-cont');
+        ticketCont.innerHTML = `<i class = "material-icons done">done</i>
+           <div class = "task-area">${task}</div>
+           <i class = "material-icons delete">delete</i>`
+
+           mainCont.appendChild(ticketCont);
+    }
     
+  
    
 })
 
