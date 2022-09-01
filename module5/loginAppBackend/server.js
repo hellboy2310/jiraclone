5a1d7fc08d883924dcc4f9216eef0b91429cf16f
hellboy2310 <bhaveshpurohit2310@gmail.com>
2022-09-01 23:22:00 +0530

    data added to mongodb using post method

--- module5/loginAppBackend/server.js
+++ module5/loginAppBackend/server.js
@@ -1,8 +1,37 @@
 const express = require("express");
-
 const app = express();
 
 const userModel = require("./userModel");
+app.use(express.json());
+//signup input
+//name
+//password
+//confirm password
+//email
+//phone
+//pic
+
+app.post("/signup", async function(req,res){
+    try{
+        let data = req.body;
+        console.log(data);
+         let newUser = await userModel.create(data);
+    
+        res.json({
+            message:"data received",
+            data:data
+        })
+    
+    }
+   catch(err){
+    console.log(err.message);
+    res.send(err.message);
+   }
+})
+
+
+
+
 app.listen(3000,function(req,res){
     console.log("server started at 3000")
 })
--- module5/loginAppBackend/userModel.js
+++ module5/loginAppBackend/userModel.js
@@ -1,10 +1,57 @@
 
 const mongoose = require('mongoose');
+const {Schema } = mongoose;
+const pass = require("./secrets");
 
-let dbLink =  `mongodb+srv://dbUser:Cgd8lbLa5BkNVDVb@cluster0.ldaghnx.mongodb.net/?retryWrites=true&w=majority`
+let dbLink =  `mongodb+srv://dbUser:${pass}@cluster0.ldaghnx.mongodb.net/?retryWrites=true&w=majority`
 
 mongoose.connect(dbLink).then(function(){
     console.log("connected");
 }).catch(function(err){
     console.log("erorr",err);
 })
+
+
+
+//how to create a schema -> only entries will be added to you schema
+
+let userSchema = new Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    confirmPassword:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    phone:{
+        type:String,
+        minLength:10,
+        maxLength:10
+    },
+    pic:{
+        type:String,
+        default:"logo2.png"
+    },
+    address:{
+        type:String
+    }
+
+})
+
+//now we have to convert it into a model so that we can work on it
+//model will be responsible to create document and read document
+
+const userModel = mongoose.model('LoginUserModel',userSchema);
+module.exports = userModel;
+ 
+
