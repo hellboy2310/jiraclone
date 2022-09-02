109f86e845401a50f25053451fee25568f1f59e6
hellboy2310 <bhaveshpurohit2310@gmail.com>
2022-09-02 13:20:19 +0530

    reset password with expiry

--- module5/loginAppBackend/server.js
+++ module5/loginAppBackend/server.js
@@ -5,6 +5,11 @@ const app = express();
 
 const cookieParser = require("cookie-parser");
 
+//npm i jsonwebtoken
+var jwt = require('jsonwebtoken');
+const secretKey = "ao98rey3hiaueir49";
+
+
 app.use(cookieParser());
 
 
@@ -45,9 +50,12 @@ app.post("/login",async function(req,res){
         if(email && password)
         {
             let user = await userModel.findOne({email:email});
+            console.log(user);
             if(user){
                 if(user.password == password){
-                    res.cookie("token","sample value");
+                    const token = jwt.sign({data:user['_id']},secretKey);
+                    console.log(token);
+                    res.cookie("JWT",token);
                     res.send("User logged in ");
                 }
                 else{
@@ -68,9 +76,115 @@ app.post("/login",async function(req,res){
     }
 })
 
-app.get("/users",function(req,res){
-    console.log(req.cookies);
+app.patch("/forgetPassword", async function(req,res){
+try{
+    let {email} = req.body;
+    let afterFiveMin = Date.now() + 1000*60*5;
+    let otp = otpGenerator();
+    //mail ke basis par search ho raha he
+    //by default->find and update ->not udpated send document
+    //new = true _. we will get updated doc
+    let user = await userModel.findOneAndUpdate({email:email},{otp:otp,otpExpiry:afterFiveMin},{new:true});
+    console.log(user);
+    res.json({
+        data:user,
+        message:"opt sent to you mail"
+    })
+    
+}
+catch(err){
+    res.send(err.message);
+}
+    
 })
+app.patch("/resetPassword",async function(req,res){
+try{
+let {otp,password,confirmPassword,email} = req.body;
+let user = await userModel.findOne({email});
+let currentTime = Date.now();
+if(currentTime > user.otpExpiry){
+    delete user.otp;
+    delete user.otpExpiry;
+    await user.save();
+    res.json({
+        message:"OTP Expired"
+    })
+}else{
+    if(user.otp != otp){
+        res.json({
+            message:"OTP does not match"
+        })
+    }
+    else{
+        user = await userModel.findOneAndUpdate({otp},{password,confirmPassword},{runValidators:true,new:true});
+        delete user.otp;
+        delete user.otpExpiry;
+        await user.save();
+
+        res.json({
+            user:user,
+            message:"user password reset complete"
+        })
+
+    }
+}
+
+
+}
+catch(err){
+   res.send(err.message);
+}
+
+})
+
+function otpGenerator(){
+    return Math.floor(Math.random() * 1000000);
+}
+
+
+
+app.get("/users",protectRoute,async function(req,res){
+    try{
+        let users = await userModel.find();
+        res.json(users);
+
+    }   
+    catch(err){
+        res.send(err.message);
+    }
+})
+
+app.get("/user",protectRoute,async function(req,res){
+    const userId = req.userId;
+    const user = await userModel.findById(userId);
+    res.json({
+        data:user,
+        message:"data about logged in user is send"
+    })
+})
+
+function protectRoute(req,res,next){
+    try{
+        let cookies = req.cookies;
+        let JWT = cookies.JWT;
+        if(cookies.JWT){
+            const token = jwt.verify(JWT,secretKey);
+            console.log(token);
+            let userId = token.data;
+            req.userId = userId;
+
+            next();
+        }
+        else{
+            res.send("you are not logged in kindly login");
+        }
+    }
+    catch(err){
+        console.log(err);
+        res.send(err.message);
+    }
+}
+
 
 app.listen(3000,function(req,res){
     console.log("server started at 3000")
